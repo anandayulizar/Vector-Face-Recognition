a27be9af8dd60082e72072a64f55ad3710475f34
Ananda Yulizar <anandayulizar@gmail.com>
2019-11-07 13:59:52 +0700

    Bersihin lagi

--- GUI_Face_Recognition.py
+++ GUI_Face_Recognition.py
@@ -5,7 +5,7 @@ import tkinter.font
 import tkinter.filedialog
 from FeaturesExtractor import *
 
-#####################################################################################
+# Inisialisasi Variabel global awal
 matchtemp = float(0)
 matchrate = float(0)
 filename = ''
@@ -31,21 +31,14 @@ def MatchUp():
 
     ma = Matcher('features_referensi.pck')
     
-    print('Query image ==========================================')
-    print(filename)
-    print(var.get())
     op = var.get()
-    print(NumPhoto.get())
     names, match = ma.match(dataUji, y, op)
-    print('Result images ========================================')
     match_rate = float(0);
     sum = float(0);
     i = 0;
     for i in range(y):
-        print('Match %s' % (match[i]))
         sum = sum + match[i]
         match_rate = sum / (i+1)
-        print(os.path.join(images_path, names[i]))
         changeDataRef(os.path.join(images_path, names[i]))
     EFinished.delete(0,'end')
     EQueue.delete(0,'end')
@@ -57,7 +50,7 @@ def MatchUp():
     EMatching.insert(0,match[i])
     EMatchRate.insert(0,match_rate)
     EHasilAkhir.insert(0,'Mirip')
-#############################################################################
+
 def NextImage():
 
     global y
@@ -69,16 +62,6 @@ def start():
     global y
     y = 1
     MatchUp()
-
-
-root = tkinter.Tk()
-root.geometry('1024x768')
-root.title("IF2123 - Aljabar Linear dan Geometri - Face Recognition")
-#root.iconbitmap("C:/Users/micha/Documents/GitHub/algeosantuy/itb.ico")
-
-# INISIALISASI FOTO AWAL
-photo = ImageTk.PhotoImage(file="EmptyPhoto.jpg")
-photo1 = ImageTk.PhotoImage(file="EmptyPhoto.jpg")
 
 # GANTI FOTO DALAM GUI
 def changeDataUji(filename):
@@ -95,6 +78,47 @@ def changeDataRef(namafile):
 
 def helloCallBack():
     tkinter.messagebox.showinfo( "Hello Python", "Hello World")
+
+def resetSetting():
+    global y
+    EFinished.delete(0,'end')
+    EQueue.delete(0,'end')
+    EHasilAkhir.delete(0,'end')
+    EMatchRate.delete(0,'end')
+    EMatching.delete(0,'end')
+    scaleNumPhoto.set(1)
+    y = 1
+    pathlabel.config(text='')
+    changeDataRef('EmptyPhoto.jpg')
+    changeDataUji('EmptyPhoto.jpg')
+    
+
+def sel():
+    selection = "Kamu memilih opsi perhitungan ke-" + str(var.get())
+    label.config(text = selection)
+
+def browsefunc():
+    global filename
+    filename = tkinter.filedialog.askopenfilename()
+    changeDataUji(filename)
+    imgName = filename.split("/")
+    pathlabel.config(text=imgName[-1])
+
+def callback():
+    if tkinter.messagebox.askyesno('Verify', 'Do you want to really quit?'):
+        quit()
+    else:
+        tkinter.messagebox.showinfo('No', 'Quit has been cancelled')
+
+root = tkinter.Tk()
+root.geometry('1024x768')
+root.title("IF2123 - Aljabar Linear dan Geometri - Face Recognition")
+#root.iconbitmap("C:/Users/micha/Documents/GitHub/algeosantuy/itb.ico")
+
+# INISIALISASI FOTO AWAL
+photo = ImageTk.PhotoImage(file="EmptyPhoto.jpg")
+photo1 = ImageTk.PhotoImage(file="EmptyPhoto.jpg")
+
 
 # Image Position in GUI
 LImage1 = Label(root, image=photo)
@@ -128,24 +152,7 @@ L5.place(x=750, y=40, in_=root)
 L6.place(x=750, y=60, in_=root)
 L7.place(x=750, y=80, in_=root)
 
-def resetSetting():
-    global y
-    EFinished.delete(0,'end')
-    EQueue.delete(0,'end')
-    EHasilAkhir.delete(0,'end')
-    EMatchRate.delete(0,'end')
-    EMatching.delete(0,'end')
-    scaleNumPhoto.set(1)
-    y = 1
-    pathlabel.config(text='')
-    changeDataRef('EmptyPhoto.jpg')
-    changeDataUji('EmptyPhoto.jpg')
-    
-
-def sel():
-    selection = "Kamu memilih opsi perhitungan ke-" + str(var.get())
-    label.config(text = selection)
-
+# Radio Button Metode Euclidian/Cosine
 LRadio = Label(root, text="Metode Perhitungan")
 LRadio['font'] = helv14
 LRadio.place(x=130, y=430, in_=root)
@@ -170,13 +177,6 @@ LName.place(x=430, y=430, in_=root)
 # EName = Entry(root, bd =2, width = 18)
 # EName['font'] = helv12
 # EName.place(x=430, y=460, in_=root)
-
-def browsefunc():
-    global filename
-    filename = tkinter.filedialog.askopenfilename()
-    changeDataUji(filename)
-    imgName = filename.split("/")
-    pathlabel.config(text=imgName[-1])
 
 browsebutton = Button(root, text="Browse", command=browsefunc)
 browsebutton['font'] = helv14
@@ -239,14 +239,6 @@ HasilAkhir.place(x=700, y=580, in_=root)
 EHasilAkhir = Entry(root, bd =2, width = 14)
 EHasilAkhir['font'] = helv12
 EHasilAkhir.place(x=800, y=580, in_=root)
-def answer():
-    tkinter.messagebox.showerror("Answer", "Sorry, this button is under maintenance")
-
-def callback():
-    if tkinter.messagebox.askyesno('Verify', 'Do you want to really quit?'):
-        quit()
-    else:
-        tkinter.messagebox.showinfo('No', 'Quit has been cancelled')
 
 MatchButton = tkinter.Button(root, height = 2, width = 10, bg='green yellow', text ="Match Up!", command = start)
 MatchButton['font'] = helv12
@@ -259,10 +251,6 @@ ResetButton.place(x=480, y=180, in_=root)
 QuitButton = tkinter.Button(root, height = 2, width = 10, bg='red', text ="Quit", command = callback)
 QuitButton['font'] = helv12
 QuitButton.place(x=480, y=320, in_=root)
-
-def increment():
-    global i
-    i = i + 1
 
 NextButton = tkinter.Button(root, height = 2, width = 10, bg='blue', text ="Next", command = NextImage)
 NextButton['font'] = helv12
